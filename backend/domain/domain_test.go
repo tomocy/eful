ff48230652b4c929beeddf02fe9c76563eacdac2
tomocy <tomocy.dev@gmail.com>
2020-02-19 01:33:06 +0900

    add utils

--- backend/domain/domain_test.go
+++ backend/domain/domain_test.go
@@ -2,6 +2,28 @@ package domain
 
 import "fmt"
 
+func assertCourse(actual, expected *Course) error {
+	if actual.Name != expected.Name {
+		return reportUnexpected("name", actual.Name, expected.Name)
+	}
+	if err := assertCoursePrice(actual.Price, expected.Price); err != nil {
+		return fmt.Errorf("unexpected price: %w", err)
+	}
+
+	return nil
+}
+
+func assertCoursePrice(actual, expected *CoursePrice) error {
+	if actual.Amount != expected.Amount {
+		return reportUnexpected("amount", actual.Amount, expected.Amount)
+	}
+	if actual.Currency != expected.Currency {
+		return reportUnexpected("currency", actual.Currency, expected.Currency)
+	}
+
+	return nil
+}
+
 func reportUnexpected(field string, actual, expected interface{}) error {
 	return fmt.Errorf("unexpected %s: got %v, expect %v", field, actual, expected)
 }
